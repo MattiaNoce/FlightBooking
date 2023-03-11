1adbe7ab8a23bce6353882fcf61a126d28837de9
MattiaNoce <mattia.noce.5@gmail.com>
2023-03-11 15:17:57 +0100

    create methods in FlightController & FlightControllerImpl

--- src/main/java/it/alten/flightbooking/controller/FlightController.java
+++ src/main/java/it/alten/flightbooking/controller/FlightController.java
@@ -1,6 +1,34 @@
 package it.alten.flightbooking.controller;
 
+import it.alten.flightbooking.models.Model.Flight;
+import it.alten.flightbooking.models.dto.FlightDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/flights")
 public interface FlightController {
+    @PostMapping()
+    ResponseEntity<Flight> save(@RequestBody FlightDto flightDto);
 
+    @GetMapping("/{id}")
+    ResponseEntity<Flight> findById(@PathVariable("id") Long id);
 
+    @GetMapping()
+    ResponseEntity<List<Flight>> findAll();
+
+    @GetMapping("/{findByFlightNumber}")
+    ResponseEntity<Flight> findByFlightNumber(@PathVariable("flightNumber") String flightNumber);
+
+    @PutMapping("/updateFlight/{id}")
+    ResponseEntity<Flight> updateUser(@PathVariable(value = "id") Long id, @RequestBody FlightDto flightDto);
+
+    @PutMapping("/updateFlightByNumber/{flightNumber}")
+    ResponseEntity<Flight> updateUser(@PathVariable(value = "flightNumber") String flightNumber, @RequestBody FlightDto flightDto);
+
+    @DeleteMapping("/delete/{id}")
+    ResponseEntity<Flight> deleteFlight(Long id);
+    @DeleteMapping("/delete")
+    ResponseEntity<List<Flight>> deleteAll();
 }
--- src/main/java/it/alten/flightbooking/controller/FlightControllerImpl.java
+++ src/main/java/it/alten/flightbooking/controller/FlightControllerImpl.java
@@ -1,5 +1,58 @@
 package it.alten.flightbooking.controller;
 
+import it.alten.flightbooking.models.Model.Flight;
+import it.alten.flightbooking.models.dto.FlightDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public class FlightControllerImpl implements  FlightController{
 
+    @Override
+    public ResponseEntity<Flight> save(FlightDto flightDto) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Flight> findById(Long id) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Flight>> findAll() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Flight> findByFlightNumber(String flightNumber) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Flight> updateUser(Long id, FlightDto flightDto) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Flight> updateUser(String flightNumber, FlightDto flightDto) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Flight> deleteFlight(Long id) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Flight>> deleteAll() {
+        //TODO
+        return null;
+    }
 }
